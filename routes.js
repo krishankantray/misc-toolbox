a5d0b1f4166f379b02be6936e6f63586909bb5a3
Krishankant Ray <krishankantray@paisabazaar.com>
2022-11-09 16:30:32 +0530

    initial commit

---
+++ routes.js
@@ -0,0 +1,45 @@
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    layout: "/admin",
+  },
+  {
+    path: "/new-post",
+    name: "New Post",
+    icon: "ni ni-planet text-blue",
+    layout: "/admin",
+  },
+  {
+    path: "/all-posts",
+    name: "All Posts",
+    icon: "ni ni-pin-3 text-orange",
+    layout: "/admin",
+  },
+  {
+    path: "/home-page",
+    name: "Homepage",
+    icon: "ni ni-single-02 text-yellow",
+    layout: "/admin",
+  },
+  {
+    path: "/tools",
+    name: "Tools",
+    icon: "ni ni-bullet-list-67 text-red",
+    layout: "/admin",
+  },
+  // {
+  //   path: "/login",
+  //   name: "Login",
+  //   icon: "ni ni-key-25 text-info",
+  //   layout: "/auth",
+  // },
+  // {
+  //   path: "/register",
+  //   name: "Register",
+  //   icon: "ni ni-circle-08 text-pink",
+  //   layout: "/auth",
+  // },
+];
+export default routes;
